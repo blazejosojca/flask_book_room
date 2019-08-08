f6a546b94af652f78297f9f7b0862c3b585332cd
Błażej Osojca <b.osojca@medicalgorithmics.com>
2019-08-08 10:45:30 +0200

    modified form of booking, new fields and babel conf

--- app/booking/forms.py
+++ app/booking/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from flask_babel import _
-from wtforms import SubmitField, TextAreaField
+from wtforms import SubmitField, TextAreaField, StringField
 from wtforms.validators import DataRequired, Length
 from wtforms.fields.html5 import DateField
 
 
 class BookingForm(FlaskForm):
-    booking_date = DateField('Date of booking', validators=[DataRequired()])
-    description = TextAreaField("Description", validators=[Length(max=130)])
+    title = StringField(_('Meeting title:', validators=[]))
+    meeting_date = DateField(_('Date of meeting', validators=[DataRequired()]))
+    description = TextAreaField(_("Description", validators=[Length(max=130)]))
     submit = SubmitField(_("Done!"))
