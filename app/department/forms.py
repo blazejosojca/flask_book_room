c57cc3b1b71825183cf52050cdad98b80f31ac1d
blazejosojca <blazejosojca@gmail.com>
2019-08-09 19:31:04 +0200

    deprartment CRUD added templates, added routes, modified form

--- app/department/forms.py
+++ app/department/forms.py
@@ -7,4 +7,4 @@ from wtforms.validators import DataRequired, Length
 class DepartmentForm(FlaskForm):
     name = StringField(_('Enter name of department', validators=[DataRequired(), Length(60)]))
     description = StringField(_('Short description', validators=[DataRequired(), Length(120)]))
-    submit = SubmitField(_("Done!"))
+    submit = SubmitField(_('Done!'))
--- app/department/routes.py
+++ app/department/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import url_for, render_template, flash
+from flask import url_for, render_template, flash, request
 from flask_login import current_user, login_required
 
 from werkzeug.utils import redirect
@@ -14,14 +14,13 @@ from app.department.forms import DepartmentForm
 @login_required
 def create_department():
     """
-    Add department to db
-    :return:
+    Add department to db.
     """
     form = DepartmentForm()
     if form.validate_on_submit():
         department = Department(
-            department_name = form.name.data,
-            description = form.data.data
+            department_name=form.name.data,
+            description=form.data.data
         )
         db.session.add(department)
         db.session.commit()
@@ -29,29 +28,47 @@ def create_department():
         flash('New department was added!')
         return redirect(url_for('main.home'))
     return render_template('department/create_department.html',
-                           title='create_department',
+                           title='Add new department',
                            form=form,
                            legend='New department'
                            )
 
-@bp.route('/booking/<int:booking_id>', methods=['GET', 'POST'])
+
+@bp.route('/department/<int:department_id>', methods=['GET', 'POST'])
 @login_required
-def view_department(booking_id):
-    
-    department = Department.query.get(booking_id)
+def department_details(department_id):
+    department = Department.query.get(department_id)
+    return render_template('department/department.html',
+                           title='Department details',
+                           department=department,
+                           legend='Department')
 
-    return render_template('booking/booking.html', booking=booking)
 
-
-
-@bp.route('/booking/delete/<int:booking_id>', methods=['GET', 'POST'])
+@bp.route('/department/list', methods=['GET', 'POST'])
 def department_list():
-    pass
+    departments = Department.query.all()
+    return render_template('department/list_departments.html',
+                           departments=departments,
+                           title='Departments list',
+                           legend='List of departments'
+                           )
 
 
-@bp.route('/booking/list/<int:room_id>', methods=['GET', 'POST'])
-def edit_department():
-    pass
+@bp.route('/department/edit/<int:department_id>', methods=['GET', 'POST'])
+def edit_department(department_id):
+    department = Department.query.get_or_404(department_id)
+    form = DepartmentForm()
+    if form.validate_on_submit():
+        department.department_name = form.department_name.data
+        department.description = form.description.data
+        db.session.commit()
+    elif request.method == 'GET':
+        department.department_name = form.department_name
+        department.description = form.description
+    return render_template('department/edit_department.html',
+                           title='Edit department info',
+                           form=form,
+                           legend='Edit department')
 
 
 @bp.route('/booking/list/<int:user_id>', methods=['GET', 'POST'])
